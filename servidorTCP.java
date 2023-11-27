f51e45b8d70c3395ad238574418798670a8456fb
marlon1925 <marlon.lalangui@epn.edu.ec>
2023-11-27 16:24:53 -0500

    servidor

--- servidorTCP.java
+++ servidorTCP.java
@@ -1,25 +1,30 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class servidorTCP {
+
     public static void main(String[] args) {
-        // crear socket del servidor
-        try {
+        // Crear socket del servidor
+        try{
             ServerSocket socket_servidor = new ServerSocket(3000);
-            System.out.println("Esperando conexiones...");
-            // Esperar y aceptar
-            while (true) {
+            System.out.println("Esperando conexion...");
+
+            while (true){
+
+                // Esperar y aceptar conxione del cliente
                 Socket socket_cliente = socket_servidor.accept();
-                System.out.println("Cliente conectado: " + socket_cliente.getInetAddress().getHostAddress());
-                // Crear un hilo par amanejar la conexion con el cliente
+                System.out.println("Cliente conectado: "+ socket_cliente.getInetAddress().getHostAddress());
+
+                // Crear un hilo para manejar la conexion con el clientee
                 hiloCliente hilo = new hiloCliente(socket_cliente);
                 hilo.start();
             }
 
-        } catch (IOException e) {
+        }catch(IOException e){
+            // Todo Auto generated cathc block
             e.printStackTrace();
         }
-
     }
 }
